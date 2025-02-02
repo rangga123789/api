d686f5ebecde0006a23334d3cb6d124f6a633b8b
ranggabaruterbit <91357564+rangga123789@users.noreply.github.com>
2025-02-02 23:49:32 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const bodyParser = require("body-parser");
+const response = require("./response");
+const axios = require("axios");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/webhook", (req, res) => {
+  res.send(req);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
