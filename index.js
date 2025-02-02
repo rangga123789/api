d58774bd440fe347734dc04e95a970f817afe883
ranggabaruterbit <91357564+rangga123789@users.noreply.github.com>
2025-02-03 00:08:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 const bodyParser = require("body-parser");
 const response = require("./response");
 const axios = require("axios");
@@ -8,7 +8,7 @@ const axios = require("axios");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/webhook", (req, res) => {
-  res.send(req);
+  res.send(req.query);
 });
 
 app.listen(port, () => {
